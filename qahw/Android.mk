884d579a2d77fff8520f3b72b793a9ef5c224719
TH779 <tongsj2333@gmail.com>
2020-09-19 12:15:10 +0000

    audio: Drop *_COPY_HEADERS
    Signed-off-by: TH779 <tongsj2333@gmail.com>
    Change-Id: Ib97eae20a2f442929851eba1365d04847098aa93

--- qahw/Android.mk
+++ qahw/Android.mk
@@ -23,19 +23,12 @@ LOCAL_SHARED_LIBRARIES := \
 
 LOCAL_CFLAGS += -Wall -Werror
 
-LOCAL_COPY_HEADERS_TO   := mm-audio/qahw/inc
-LOCAL_COPY_HEADERS      := inc/qahw.h
-LOCAL_COPY_HEADERS      += inc/qahw_effect_api.h
+LOCAL_EXPORT_C_INCLUDE_DIRS := $(LOCAL_PATH)/inc
 
 LOCAL_PRELINK_MODULE    := false
 LOCAL_VENDOR_MODULE     := true
 
 include $(BUILD_SHARED_LIBRARY)
 
-include $(CLEAR_VARS)
-LOCAL_COPY_HEADERS_TO   := mm-audio/qahw_api/inc
-LOCAL_COPY_HEADERS      := inc/qahw_defs.h
-
-include $(BUILD_COPY_HEADERS)
 endif
 endif
