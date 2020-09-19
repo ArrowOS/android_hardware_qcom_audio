bf79fae046cfb2f58ea3b2d325c61a4ebf45bbc7
daniml3 <danimoral1001@gmail.com>
2020-09-19 15:23:22 +0200

    audio: Remove deprecated LOCAL_COPY_HEADERS
    Signed-off-by: daniml3 <danimoral1001@gmail.com>
    Change-Id: I019abd13be56f13259b8f13c978fb2ceddf176e4

--- qahw/Android.mk
+++ qahw/Android.mk
@@ -23,9 +23,7 @@ LOCAL_SHARED_LIBRARIES := \
 
 LOCAL_CFLAGS += -Wall -Werror
 
-LOCAL_COPY_HEADERS_TO   := mm-audio/qahw/inc
-LOCAL_COPY_HEADERS      := inc/qahw.h
-LOCAL_COPY_HEADERS      += inc/qahw_effect_api.h
+LOCAL_EXPORT_C_INCLUDE_DIRS := $(LOCAL_PATH)/inc
 
 LOCAL_PRELINK_MODULE    := false
 LOCAL_VENDOR_MODULE     := true
